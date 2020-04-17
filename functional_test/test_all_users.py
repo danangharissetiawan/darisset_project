c292da8880106621a6939c4ec2a9650992e4fbcf
danangharissetiawan <mr.danangharissetiawan@gmail.com>
2020-04-17 23:47:49 +0700

    Settings static files and templates

---
+++ functional_test/test_all_users.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+
+from django.urls import reverse
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+# import unittest
+
+
+class HomeNewVisitorTest(StaticLiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+    
+    def tearDown(self):
+        self.browser.quit()
+
+    def get_full_url(self, namespace):
+        return self.live_server_url + reverse(namespace)
+
+    def test_home_title(self):
+        self.browser.get(self.get_full_url("home"))
+        self.assertIn("Risset", self.browser.title)
+
+    # def test_h1_css(self):
+    #     self.browser.get(self.get_full_url("home"))
+    #     h1 = self.browser.find_element_by_tag_name("h1")
+    #     self.assertEqual(h1.value_of_css_property(
+    #         "color"), "rgba(200, 50, 255, 1)")
+
+    # def test_it_worked(self):
+    #     self.browser.get('http://localhost:8000')
+    #     self.assertIn("Django: the Web framework for perfectionists with deadlines.", self.browser.title)
+
+# if __name__ == '__main__':
+#     unittest.main(warnings='ignore')
